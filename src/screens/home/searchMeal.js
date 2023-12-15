a853c73755fd72987eeb6c9468e120f1b71c77b5
Ajay Chawada <ajay@Ajays-MacBook-Air.local>
2023-12-15 14:14:20 +0530

    Added title

--- src/screens/home/searchMeal.js
+++ src/screens/home/searchMeal.js
@@ -55,64 +55,71 @@ export default function SearchMeal({navigation}) {
     }
   }
   return (
-    <View
-      style={tailwind`flex-row m-2 justify-between items-center rounded-full`}>
-      <TouchableOpacity
-        style={tailwind`bg-slate-200 px-3 py-4 rounded-full flex-1 mr-2`}
-        onPress={() => {
-          setShowModal(true);
-        }}>
-        <Text>Search meals</Text>
-      </TouchableOpacity>
+    <>
+      <Text style={[tailwind`mx-2 font-bold text-xl`]}>Are you Hungry?</Text>
+      <Text style={[tailwind`mx-2 text-lg`]}>Let's Order, Eat & Repeat</Text>
+      <View
+        style={tailwind`flex-row m-2 justify-between items-center rounded-full`}>
+        <TouchableOpacity
+          style={tailwind`bg-slate-200 px-3 py-4 rounded-full flex-1`}
+          onPress={() => {
+            setShowModal(true);
+          }}>
+          <Text>Search meals</Text>
+        </TouchableOpacity>
 
-      <Modal visible={showModal} transparent={true}>
-        <View
-          style={[
-            tailwind`flex-1 mt-${insets.top}px bg-white shadow-xl`,
-            {borderRadius: responsiveHeight(5)},
-          ]}>
-          <TouchableOpacity
-            onPress={() => {
-              setMeals([]);
-              setSeachText('');
-              setShowModal(false);
-            }}
-            style={tailwind`mt-5 ml-auto mr-5`}>
-            <XMarkIcon size={responsiveHeight(3)} color={'#000'} />
-          </TouchableOpacity>
+        <Modal visible={showModal} transparent={true}>
           <View
-            style={tailwind`flex-row m-2 justify-between items-center rounded-full`}>
-            <TextInput
-              style={tailwind`bg-slate-200 px-3 py-4 justify-between items-center rounded-full flex-1 mr-2`}
-              value={seachText}
-              onChangeText={text => setSeachText(text)}
-              placeholder="Search meals"
-              placeholderTextColor={'#000'}
-              onSubmitEditing={searchMealData}
-            />
-            <TouchableOpacity onPress={searchMealData}>
-              <MagnifyingGlassIcon color={'#000'} size={responsiveHeight(4)} />
+            style={[
+              tailwind`flex-1 mt-${insets.top}px bg-white shadow-xl`,
+              {borderRadius: responsiveHeight(5)},
+            ]}>
+            <TouchableOpacity
+              onPress={() => {
+                setMeals([]);
+                setSeachText('');
+                setShowModal(false);
+              }}
+              style={tailwind`mt-5 ml-auto mr-5`}>
+              <XMarkIcon size={responsiveHeight(3)} color={'#000'} />
             </TouchableOpacity>
+            <View
+              style={tailwind`flex-row m-2 justify-between items-center rounded-full`}>
+              <TextInput
+                style={tailwind`bg-slate-200 px-3 py-4 justify-between items-center rounded-full flex-1 mr-2`}
+                value={seachText}
+                onChangeText={text => setSeachText(text)}
+                placeholder="Search meals"
+                placeholderTextColor={'#000'}
+                onSubmitEditing={searchMealData}
+              />
+              <TouchableOpacity onPress={searchMealData}>
+                <MagnifyingGlassIcon
+                  color={'#000'}
+                  size={responsiveHeight(4)}
+                />
+              </TouchableOpacity>
+            </View>
+            {meals?.length > 0 && (
+              <MasonryLayout
+                meals={meals}
+                navigation={navigation}
+                animationType="slide"
+                onPressMeal={() => setShowModal(false)}
+              />
+            )}
+            {loadingStatus.loading && <ActivityIndicator />}
+
+            {!meals?.length &&
+              !loadingStatus.loading &&
+              loadingStatus.hasLoadedOnce && <Text>No Meal found</Text>}
+
+            {!meals?.length && !loadingStatus.hasLoadedOnce && (
+              <Text>Search Meal</Text>
+            )}
           </View>
-          {meals?.length > 0 && (
-            <MasonryLayout
-              meals={meals}
-              navigation={navigation}
-              animationType="slide"
-              onPressMeal={() => setShowModal(false)}
-            />
-          )}
-          {loadingStatus.loading && <ActivityIndicator />}
-
-          {!meals?.length &&
-            !loadingStatus.loading &&
-            loadingStatus.hasLoadedOnce && <Text>No Meal found</Text>}
-
-          {!meals?.length && !loadingStatus.hasLoadedOnce && (
-            <Text>Search Meal</Text>
-          )}
-        </View>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
+    </>
   );
 }
