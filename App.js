368cada2baa0a12c2e78889203b24c9252cc07fb
Ajay Chawada <ajay@Ajays-MacBook-Air.local>
2024-02-04 13:08:58 +0530

    Added spash screen and app icon for ios

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeRoute from './src/routes/home.route';
 import DrawerLayout from './src/components/drawerLayout';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <DrawerNavigation />
